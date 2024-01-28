e43d04c9d9ee81aff93ab3be24827fcbf7e92d3e
daryldixon1994 <kmohamedanouar@gmail.com>
2024-01-28 16:55:56 +0100

    update cors settings

--- server.js
+++ server.js
@@ -20,15 +20,25 @@ mongoose
 
 // global middlewares
 app.use(express.json());
-app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+var allowlist = ["http://localhost:3000", "https://kampina.netlify.app/"];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+app.use(cors(corsOptionsDelegate));
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // user routes
